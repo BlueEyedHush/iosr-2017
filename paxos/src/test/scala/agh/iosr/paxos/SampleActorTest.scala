4ec940d03aed96404b6918ef9dcd2cc2591a9fb0
knawara <blueeyedhush@gmail.com>
2017-11-07 18:29:15 +0100

    example actor & his test

---
+++ paxos/src/test/scala/agh/iosr/paxos/SampleActorTest.scala
@@ -0,0 +1,24 @@
+package agh.iosr.paxos
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class SampleActorTest extends TestKit(ActorSystem("MySpec")) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Echo actor" must {
+
+    "send back messages unchanged" in {
+      val echo = system.actorOf(Props[SampleActor])
+      echo ! "hello world"
+      expectMsg("hello world")
+    }
+
+  }
+
+}
