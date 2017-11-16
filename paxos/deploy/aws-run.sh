61f39e3d0b224be3d371c40ab188970d11ef7306
knawara <blueeyedhush@gmail.com>
2017-11-16 12:47:52 +0100

    [AWS-DEPLOY] create folder for logs on remote machines; stdin/stdout detach in run scirpt; make aws-run executable; add console appender

--- paxos/deploy/aws-run.sh
+++ paxos/deploy/aws-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -Dconfig.file="/home/ubuntu/application.conf" -jar /home/ubuntu/paxos-iosr.jar
+java -Dconfig.file="/home/ubuntu/application.conf" -jar /home/ubuntu/paxos-iosr.jar < /dev/null &> out.stdlog &
